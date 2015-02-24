2ae6f7c0a275c97911890d0529f9a5b4cdf3a184
Rory Carson <roryscarson@gmail.com>
2015-02-24 18:13:52 +0400

    Scraped and cleaned up lyrics.
    Adding the send_tweet code for future reference.

--- scrape_lyrics.py
+++ scrape_lyrics.py
@@ -1,35 +1,26 @@
 import requests
 import bs4
 
-root_url = 'http://www.metrolyrics.com/'
-url = 'http://www.metrolyrics.com/ride-lyrics-lana-del-rey.html'
-song_list_url1 = 'http://www.metrolyrics.com/lana-del-rey-alpage-1.html'
-song_list_url2 = 'http://www.metrolyrics.com/lana-del-rey-alpage-2.html'
-song_list_url3 = 'http://www.metrolyrics.com/lana-del-rey-alpage-3.html'
+root_url = 'http://lyrics.wikia.com'
+artist_url = '/Lana_Del_Rey'
+
 
 #returns a list of all song links
-#originally designed for a different lyrics site.
-#TODO update to work with metrolyrics
-def get_lyrics_page_urls():
-	response = requests.get(url)
+def get_lyrics_page_urls(artist_url):
+	response = requests.get(root_url + artist_url)
 	soup = bs4.BeautifulSoup(response.text)
-	return [a.attrs.get('href') for a in soup.select('#listAlbum a[href^=../lyrics/lanadelrey]')]
+	return [a.attrs.get('href') for a in soup.select('ol a')]
 
 def get_song_info(lyrics_page_url):
 	song_data = {}
-	response = requests.get(lyrics_page_url)
-	#response = requests.get(root_url + lyrics_page_url)
+	response = requests.get(root_url + lyrics_page_url)
 	soup = bs4.BeautifulSoup(response.text)
-	song_data['title'] = soup.select('h1')[0].get_text().strip()
-	song_data['lyrics'] = [p.get_text().strip() for p in soup.select('.verse')] 	
+	[s.extract() for s in soup('script')] #removes scripts surrounding lyrics
+	song_data = [p.get_text().strip() for p in soup.select('.lyricbox')] 	
 	return song_data
 
-print get_song_info(url)
+lyrics_page_urls = get_lyrics_page_urls(artist_url)
+for lyrics_page_url in lyrics_page_urls:
+		print get_song_info(lyrics_page_url)
 
 
-#lyrics_page_urls = get_lyrics_page_urls()
-#for lyrics_page_url in lyrics_page_urls:
-#		print get_song_info(lyrics_page_url)
-
-
-#print(get_lyrics_page_urls())
